5331eb39f5831a9137a94da7f992fbb279e814b6
jcLucio <c.lucio3@gmail.com>
2021-08-19 23:49:41 -0500

    Add /new routes - Add GET /new route to render form - Add POST /new route to add new message

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,36 @@ const messages = [
     user: 'Charles',
     added: new Date(),
   },
+  {
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit ad iusto consectetur quo, incidunt modi voluptates eos mollitia dolorem minus nostrum ipsam, impedit fugiat dolor omnis animi in tenetur ratione.',
+    user: 'Yoshi',
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
 router.get('/', (_req, res) => {
   res.render('index', { title: 'Mini message board', messages });
+});
+
+/* GET create new message page. */
+router.get('/new', (_req, res) => {
+  res.render('form');
+});
+
+/* POST new message page. */
+router.post('/new', (req, res) => {
+  const { author, messageText } = req.body;
+
+  const newMessage = {
+    text: messageText,
+    user: author,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  res.redirect('/');
 });
 
 module.exports = router;
